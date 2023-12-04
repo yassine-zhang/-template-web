1af39f3d6a23fb87771950c84c70dc9fc91fa439
ZhangSir <57878778@qq.com>
2023-12-04 15:27:24 +0800

    feat: added 'ant-design-vue' and 'tailwindcss' ui framework

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-2xl lg:text-3xl">This is an about page</h1>
+
+    <div class="mt-3">
+      <a-collapse v-model:activeKey="activeKey">
+        <a-collapse-panel key="1" header="This is panel header 1">
+          <p>{{ text }}</p>
+        </a-collapse-panel>
+        <a-collapse-panel key="2" header="This is panel header 2">
+          <p>{{ text }}</p>
+        </a-collapse-panel>
+        <a-collapse-panel
+          key="3"
+          header="This is panel header 3"
+          collapsible="disabled"
+        >
+          <p>{{ text }}</p>
+        </a-collapse-panel>
+      </a-collapse>
+    </div>
   </div>
 </template>
 
@@ -9,7 +27,20 @@
   .about {
     min-height: 100vh;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
   }
 }
 </style>
+<script setup>
+import { Collapse, CollapsePanel } from "ant-design-vue";
+import { ref, watch } from "vue";
+const text = `A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.`;
+const activeKey = ref(["1"]);
+watch(activeKey, (val) => {
+  console.log(val);
+});
+
+const ACollapse = Collapse;
+const ACollapsePanel = CollapsePanel;
+</script>
